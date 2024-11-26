cb4a51e9a1eafe476f9c2794fda0cbaa1622aaab
Eric Marty <emarty@uga.edu>
2024-11-26 16:34:08 -0500

    Adapt CEID_walk for flusight 2024-2025. This code is functional but still shop code. Needs to be cleaned up.

--- FluSight.R
+++ FluSight.R
@@ -15,47 +15,49 @@ read_forecast <- function(file) {
 }
 
 quant <- function(x, p){
-  quantile(x, prob = p, names = FALSE, type = 8, na.rm = TRUE)
+  quantile(x, prob = p, names = FALSE, 
+           type = 8, # see ?quantile for types
+           na.rm = TRUE)
 }
 
-vardf <- function(var, samp){
-  cname <- switch(var,
-                  "inc death" = "deaths",
-                  "inc case" = "cases")
-  if (var != "inc case") {
-    prob <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
-  } else {
-    prob <- c(0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975)
-  }
+vardf_2024_2025 <- function(var, samp){
+  # cname <- switch(var,
+  #                 "inc death" = "deaths",
+  #                 "inc case" = "cases")
+  prob <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
   t1 <- tibble(
     target = var,
     type  = "quantile",
     quantile = prob,
-    value = quant(samp[[cname]], prob)
+    # value = quant(samp[[cname]], prob)
+    value = quant(samp[[var]], prob)
   )
   t2 <-
     tibble(
       target = var,
       type = "point",
       quantile = NA,
-      value = quant(samp[[cname]], 0.5)
-      
+      # value = quant(samp[[cname]], 0.5)
+      value = quant(samp[[var]], 0.5)
     )
   bind_rows(t1, t2)
 }
 
-samp_to_df <-
+samp_to_df_2024_2025 <-
   function(sampdf,
-           vars = c("inc death", "inc case")) {
-    purrr::map_dfr(vars, vardf, samp = sampdf)
+           vars
+           # vars = c("inc death", "inc case")
+  ) {
+    purrr::map_dfr(vars, vardf_2024_2025, samp = sampdf)
   }
 
 # Take simulation trajectories and output a data frame in the format described
 # here: https://github.com/reichlab/covid19-forecast-hub/blob/6a7e5624ef540a55902770b7c17609d19e1f593a/data-processed/README.md
-paths_to_forecast <- function(out, loc = "13", wks_ahead = 1:6, hop, fdt) {
+paths_to_forecast_2024_2025 <- function(out, loc = "13", wks_ahead = 1:6, target_types, hop, fdt) {
   if(any(wks_ahead > 20)){
     stop("Max weeks ahead accepted is 20", .call = FALSE)
   }
+  cat(paste(target_types,"\n")) ### DEBUG
   
   out2 <- 
     out %>% group_by(Rep) %>% arrange(Date) %>% 
@@ -82,28 +84,34 @@ paths_to_forecast <- function(out, loc = "13", wks_ahead = 1:6, hop, fdt) {
     week0_sun <- fdt_sun
   }
   forecast_epiweeks <- (week0_sun + wks_ahead * 7) %>% lubridate::epiweek()
+  print(paste("EPIWEEKS:", forecast_epiweeks)) ### DEBUG
   if(any(na.omit(out2$day_diff) > 7)){
     stop("Non-continuous series of weeks, unable to compute cumulative forecasts", 
          .call = FALSE)
   }
   
+  nesting_vars <- c("Rep", target_types)
+  # print(nesting_vars) ### DEBUG
+  # return(out2) ### DEBUG
+  
   weekly <- out2 %>%
     as_tibble() %>%
     mutate(epiweek = lubridate::epiweek(Date)) %>%
-    filter(epiweek %in% forecast_epiweeks) %>%
+  # return(weekly) ### DEBUG
+    filter(epiweek %in% forecast_epiweeks) %>% ## RETURNS EMPTY DF
     rename("target_end_date" = Date) %>%
-    nest(data = c(Rep, cases, deaths)) %>%
-    mutate(pred_df = purrr::map(data, samp_to_df, 
-                                vars = c("inc death", "inc case"))) %>%
+    nest(data = any_of(nesting_vars)) %>%
+    mutate(pred_df = purrr::map(data, samp_to_df_2024_2025,
+                                vars = target_types)) %>%
     select(-data) %>%
     unnest(pred_df) %>%
-    mutate(target = paste((target_end_date - (week0_sun + 6)) / lubridate::ddays(7), 
+    mutate(target = paste((target_end_date - (week0_sun + 6)) / lubridate::ddays(7),
                           "wk ahead", target)) %>%
     add_column(location = loc) %>%
     mutate(quantile = round(quantile, digits = 3),
            value = round(value, digits = 3)) %>%
     add_column(forecast_date = fdt) %>%
-    select(forecast_date, target, target_end_date, location, type, quantile, 
+    select(forecast_date, target, target_end_date, location, type, quantile,
            value)
   
   #weekly %>% 
